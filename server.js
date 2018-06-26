c4f9dc3a13574fc50026ce86124d7a8fd8d5cca2
David Miller <hawky454@gmail.com>
2018-06-26 13:36:36 -0500

    Installed morgan, body parser, pg and knex

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ let app = express();
 let port = process.env.PORT || 8000;
 let morgan = require('morgan');
 let bodyParser = require('body-parser');
+
+app.disable('x-powered-by');
+app.use(morgan('short'));
+
+
+app.listen(port, function() {
+	console.log('Listening on port', port);
+});

8d683a149360b2cc11c8a8a8b18b96e0e6f64e19
David Miller <hawky454@gmail.com>
2018-06-26 16:08:53 -0500

    Added form to assassins_new and fixed the labels so that they are not wrapping around inputs

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,9 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+     client: 'pg',
+     connection: 'postgres://localhost/killbase1'
+   },
 
   staging: {
     client: 'postgresql',
